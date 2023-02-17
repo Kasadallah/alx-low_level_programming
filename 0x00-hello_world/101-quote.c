d81c21544ae3bf4b202435c2144c487479e8e84f
khaled AbdElnasser <khaled.abdelnasser17@gmail.com>
2023-02-16 18:42:06 -0800

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 /**
  * main - Entry
  * Return: 1
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" ,stdout);
+	write(STDOUT_FILENO,b"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" , 59);
 	return (1);
 }
