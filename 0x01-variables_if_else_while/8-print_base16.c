19e35145ce95eb32a10ade8aa715b54a4fb597d7
khaled AbdElnasser <khaled.abdelnasser17@gmail.com>
2023-02-17 17:38:43 -0800

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -  entry
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	int d;
+
+	c = 'a';
+	d = 0;
+	while (d < 10)
+	{
+		putchar(d + '0');
+		d++;
+	}
+	while (c <= 'f')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
