11878c9f15dd5d226fda902e0afc4e64d1d61c68
khaled AbdElnasser <khaled.abdelnasser17@gmail.com>
2023-02-17 17:25:09 -0800

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -  entry
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d\n", a)
+	return (0);
+}
