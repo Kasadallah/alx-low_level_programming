aed26cd92f4e27dc0837f47592b9fb40038b2d95
khaled AbdElnasser <khaled.abdelnasser17@gmail.com>
2023-03-09 13:30:16 -0800

    1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,24 +1,18 @@
 /*
- * File: 2-strlen_recursion.c
+ * File: 1-print_rev_recursion.c
  */
 
 #include "main.h"
 
 /**
- * _strlen_recursion - Returns the length of a string.
- * @s: The string to be measured.
- *
- * Return: The length of the string.
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: The string to be printed.
  */
-int _strlen_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-	int len = 0;
-
 	if (*s)
 	{
-		len++;
-		len += _strlen_recursion(s + 1);
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-
-	return (len);
 }
