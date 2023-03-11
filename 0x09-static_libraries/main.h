73ba2cd7ac96f41eb369f14a35ee5e65e96618f3
khaled AbdElnasser <khaled.abdelnasser17@gmail.com>
2023-03-11 14:14:52 -0800

    a

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef Main
-#define Main
+#ifndef MAIN_H
+#define MAIN_H
 
 /*
  * File: main.h
@@ -27,5 +27,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
 #endif
